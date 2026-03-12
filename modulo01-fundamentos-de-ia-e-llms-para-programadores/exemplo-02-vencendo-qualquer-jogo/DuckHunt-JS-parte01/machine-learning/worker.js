e0319edcb1df4c23d1e3beac3015050cfb11af46
Gabriel Babler <gabrielbabler@MacBook-Air-de-Gabriel-e-Leticia.local>
2026-03-12 08:43:32 -0300

    aula 2 mod 8

--- modulo01-fundamentos-de-ia-e-llms-para-programadores/exemplo-02-vencendo-qualquer-jogo/DuckHunt-JS-parte01/machine-learning/worker.js
+++ modulo01-fundamentos-de-ia-e-llms-para-programadores/exemplo-02-vencendo-qualquer-jogo/DuckHunt-JS-parte01/machine-learning/worker.js
@@ -3,6 +3,7 @@ importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
 const INPUT_MODEL_DIMENTIONS = 640
+const CLASS_THRESHOLD = 0.4
 
 let _labels = []
 let _model = null
@@ -66,6 +67,44 @@ async function runInference(tensor) {
         classes: classesData
     }
 }
+
+/**
+ * Filtra e processa as predicoes:
+ * - Aplica o limiar de confianca (CLASS_THRESHOLD)
+ * - Filtra apenas a classe desejada (exemplo: `kite`)
+ * - Converte coordenadas normalizadas para pixels reais
+ * - Calcula o centro do bounding box
+ * 
+ * Uso de generator (function *):
+ * - Permite enviar cada predicao assim que processada, sem criar lista intermediaria
+ */
+function * processPrediction({ boxes, scores, classes }, width, height) {
+    for (let index = 0; index < scores.length; index++) {
+        if(scores[index] < CLASS_THRESHOLD) continue
+
+        const label = _labels[classes[index]]
+        if(label !== 'kite') continue
+        
+        let [x1, y1, x2, y2] = boxes.slice(index * 4, (index + 1) * 4)
+        //normalizar tamanho de tela
+        x1 *= width
+        x2 *= width
+        y1 *= height
+        y2 *= height
+
+        const boxWidth = x2 - x1
+        const boxHeight = y2 - y1
+        const centerX = x1 + boxWidth / 2
+        const centerY = y1 + boxHeight / 2
+
+        yield {
+            x: centerX,
+            y: centerY,
+            score: (scores[index] * 100).toFixed(2)
+        }
+    }
+}
+
 loadModelAndLabels()
 
 self.onmessage = async ({ data }) => {
@@ -76,16 +115,13 @@ self.onmessage = async ({ data }) => {
     const { width, height } = data.image
 
     const inferenceResults = await runInference(input)
-
-    debugger
-    postMessage({
+    
+    for (const prediction of processPrediction(inferenceResults, width, height)) {
+        postMessage({
         type: 'prediction',
-        x: 400,
-        y: 400,
-        score: 0
+        ...prediction
     });
-
-
+    }
 };
 
 console.log('🧠 YOLOv5n Web Worker initialized');
